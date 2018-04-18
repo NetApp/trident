43498fd520626e557f7010138b039436036a8506
Chuck <fchuck@netapp.com>
2018-04-18 09:55:10 -0500

    Added request ID to REST API logging.

--- frontend/rest/logger.go
+++ frontend/rest/logger.go
@@ -6,18 +6,26 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/rs/xid"
 	log "github.com/sirupsen/logrus"
 )
 
 func Logger(inner http.Handler, name string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
+		requestId := xid.New()
+		logRestCallInfo("REST API call received.", r, start, requestId, name)
 		inner.ServeHTTP(w, r)
-		log.WithFields(log.Fields{
-			"method":   r.Method,
-			"uri":      r.RequestURI,
-			"route":    name,
-			"duration": time.Since(start),
-		}).Info("API server REST call.")
+		logRestCallInfo("REST API call complete.", r, start, requestId, name)
 	})
+}
+
+func logRestCallInfo(msg string, r *http.Request, start time.Time, requestId xid.ID, name string) {
+	log.WithFields(log.Fields{
+		"requestID": requestId,
+		"method":    r.Method,
+		"uri":       r.RequestURI,
+		"route":     name,
+		"duration":  time.Since(start),
+	}).Info(msg)
 }
