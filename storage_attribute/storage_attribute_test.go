69293f8a62a790967b947b96094b9422ff756c40
simrins <107222069+simrins@users.noreply.github.com>
2022-07-12 18:07:20 -0600

    Storage attribute 80% unit test coverage
    Unit test coverage 98.8%

--- storage_attribute/storage_attribute_test.go
+++ storage_attribute/storage_attribute_test.go
@@ -4,12 +4,15 @@ package storageattribute
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"reflect"
+	"sort"
 	"testing"
 
 	log "github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestMain(m *testing.M) {
@@ -211,5 +214,198 @@ func TestUnmarshalRequest(t *testing.T) {
 
 	if !reflect.DeepEqual(requestMap, targetRequestMap) {
 		t.Errorf("Maps are unequal.\n Expected: %s\nGot: %s\n", requestMap, targetRequestMap)
+	}
+}
+
+func TestNewBoolOfferFromOffers(t *testing.T) {
+	for i, test := range []struct {
+		actual   Offer
+		expected Offer
+	}{
+		{NewBoolOfferFromOffers(NewBoolOffer(true), NewBoolOffer(true)), NewBoolOffer(true)},
+		{NewBoolOfferFromOffers(NewBoolOffer(true), NewBoolOffer(false)), NewBoolOffer(true)},
+		{NewBoolOfferFromOffers(NewBoolOffer(false), NewBoolOffer(true)), NewBoolOffer(true)},
+		{NewBoolOfferFromOffers(NewBoolOffer(false), NewBoolOffer(false)), NewBoolOffer(false)},
+	} {
+		assert.Equal(t, test.expected, test.actual, fmt.Sprintf("Test case %d failed", i))
+	}
+}
+
+func TestNewStringOfferFromOffers(t *testing.T) {
+	for i, test := range []struct {
+		actual   Offer
+		expected Offer
+	}{
+		{NewStringOfferFromOffers(NewStringOffer("foo", "bar", "foo")), NewStringOffer("bar", "foo")},
+		{NewStringOfferFromOffers(NewStringOffer("foo", "bar", "baz")), NewStringOffer("bar", "baz", "foo")},
+	} {
+		if sOffer, ok := test.actual.(*stringOffer); ok {
+			sort.Strings(sOffer.Offers)
+			test.actual = sOffer
+		}
+		assert.Equal(t, test.expected, test.actual, fmt.Sprintf("Test case %d failed", i))
+	}
+}
+
+func TestGetType(t *testing.T) {
+	for i, test := range []struct {
+		actual   Request
+		expected Type
+	}{
+		{NewIntRequest(6), "int"},
+		{NewBoolRequest(true), "bool"},
+		{NewStringRequest("bar"), "string"},
+		{NewLabelRequestMustCompile("performance = gold"), "label"},
+	} {
+		assert.Equal(t, test.expected, test.actual.GetType(), fmt.Sprintf("Test case %d failed", i))
+	}
+}
+
+func TestValue(t *testing.T) {
+	for i, test := range []struct {
+		actual   Request
+		expected interface{}
+	}{
+		{NewIntRequest(6), 6},
+		{NewBoolRequest(false), false},
+		{NewStringRequest("baz"), "baz"},
+		{NewLabelRequestMustCompile("performance = gold"), "performance = gold"},
+	} {
+		assert.Equal(t, test.expected, test.actual.Value(), fmt.Sprintf("Test case %d failed", i))
+	}
+}
+
+func TestLabels(t *testing.T) {
+	offer := NewLabelOffer(map[string]string{"performance": "silver", "protection": "minimal"})
+
+	actualMap := offer.(*labelOffer).Labels()
+	expectedMap := map[string]string{"performance": "silver", "protection": "minimal"}
+
+	assert.Equal(t, expectedMap, actualMap)
+}
+
+func TestCreateBackendStoragePoolsMapFromEncodedString(t *testing.T) {
+	actualMap, _ := CreateBackendStoragePoolsMapFromEncodedString("backend1:pool1,pool2;backend2:pool3")
+	targetMap := map[string][]string{"backend1": {"pool1", "pool2"}, "backend2": {"pool3"}}
+
+	assert.Equal(t, targetMap, actualMap, fmt.Sprintf("Test case failed"))
+}
+
+func TestCreateBackendStoragePoolsMapFromEncodedStringNegative(t *testing.T) {
+	_, actualErr := CreateBackendStoragePoolsMapFromEncodedString("backend1;backend2:pool3")
+	expectedErr := fmt.Errorf("the encoded backend-storage pool string does not have the right format")
+
+	assert.Equal(t, expectedErr, actualErr, fmt.Sprintf("Test case failed"))
+}
+
+func TestBoolString(t *testing.T) {
+	boolOffer := boolOffer{
+		Offer: true,
+	}
+	assert.Equal(t, "true", boolOffer.ToString())
+	assert.Equal(t, "{Offer:  true}", boolOffer.String())
+}
+
+func TestString(t *testing.T) {
+	stringOffer := stringOffer{
+		Offers: []string{"foo", "bar", "baz"},
+	}
+
+	assert.Equal(t, "foo,bar,baz", stringOffer.ToString())
+	assert.Equal(t, "{Offers: foo,bar,baz}", stringOffer.String())
+}
+
+func TestLabelString(t *testing.T) {
+	labelOffer := labelOffer{
+		Offers: map[string]string{"performance": "silver", "protection": "minimal"},
+	}
+	assert.Equal(t, "map[performance:silver protection:minimal]", labelOffer.ToString())
+	assert.Equal(t, "{Offers: map[performance:silver protection:minimal]}", labelOffer.String())
+}
+
+func TestIntString(t *testing.T) {
+	intOffer := intOffer{
+		Min: 6,
+		Max: 10,
+	}
+	assert.Equal(t, "{Min: 6, Max: 10}", intOffer.ToString())
+}
+
+func TestNewLabelRequestNegative(t *testing.T) {
+	for i, test := range []struct {
+		requestString string
+		expectedErr   error
+	}{
+		{"", fmt.Errorf("label selector may not be empty")},
+		{"performance=gold+protection=full", fmt.Errorf("invalid label selector: performance=gold+protection=full")},
+	} {
+		_, actualErr := NewLabelRequest(test.requestString)
+		assert.Equal(t, test.expectedErr, actualErr, fmt.Sprintf("Test case %d failed", i))
+	}
+}
+
+func TestUnmarshalRequestMapNil(t *testing.T) {
+	_, actualErr := UnmarshalRequestMap(nil)
+	expectedErr := error(nil)
+
+	assert.Equal(t, expectedErr, actualErr, fmt.Sprintf("Test case failed"))
+}
+
+func TestUnmarshalRequestMapNegative(t *testing.T) {
+	for i, test := range []struct {
+		requestMap  json.RawMessage
+		expectedErr error
+	}{
+		{[]byte("bar"), fmt.Errorf(
+			"unable to unmarshal map: invalid character 'b' looking for beginning of value")},
+		{[]byte(`{"ExistentBool":"true"}`), fmt.Errorf("unrecognized storage attribute: ExistentBool")},
+	} {
+		_, actualErr := UnmarshalRequestMap(test.requestMap)
+		assert.Equal(t, test.expectedErr, actualErr, fmt.Sprintf("Test case %d failed", i))
+	}
+}
+
+func TestMarshalRequestMapNil(t *testing.T) {
+	_, actualErr := MarshalRequestMap(nil)
+	expectedErr := error(nil)
+
+	assert.Equal(t, expectedErr, actualErr, fmt.Sprintf("Test case failed"))
+}
+
+func TestCreateAttributeRequestFromAttributeValueNegative(t *testing.T) {
+	for i, test := range []struct {
+		name        string
+		val         string
+		expectedErr error
+	}{
+		{"snapshots", "No", fmt.Errorf(
+			"storage attribute value (No) doesn't match the specified type (" +
+				"bool); strconv.ParseBool: parsing \"No\": invalid syntax")},
+		{"IOPS", "123A", fmt.Errorf(
+			"storage attribute value (123A) doesn't match the specified type (int); strconv." +
+				"ParseInt: parsing \"123A\": invalid syntax")},
+		{"labels", "!performance, !protection", fmt.Errorf(
+			"storage attribute value (!performance, !protection) doesn't match the specified type (" +
+				"label); invalid label selector: !performance, !protection")},
+	} {
+		_, actualErr := CreateAttributeRequestFromAttributeValue(test.name, test.val)
+		assert.Equal(t, test.expectedErr, actualErr, fmt.Sprintf("Test case %d failed", i))
+	}
+}
+
+func TestUnmarshalOfferMapNegative(t *testing.T) {
+	for i, test := range []struct {
+		offerMap    json.RawMessage
+		expectedErr error
+	}{
+		{[]byte("bar"), fmt.Errorf(
+			"unable to unmarshal map: invalid character 'b' looking for beginning of value")},
+		{[]byte(`{"ExistentBool":"true"}`), fmt.Errorf("unknown storage attribute: ExistentBool")},
+		{[]byte(`{"IOPS":"123A"}`), fmt.Errorf(
+			"unable to fully unmarshal request IOPS: json: cannot unmarshal string into Go value of type " +
+				"storageattribute.intOffer")},
+	} {
+		_, actualErr := UnmarshalOfferMap(test.offerMap)
+		assert.Equal(t, test.expectedErr, actualErr, fmt.Sprintf("Test case %d failed", i))
 	}
 }
