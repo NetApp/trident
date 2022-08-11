6982405726b3ffdde709cc3b3172296d4bf12145
Joseph Webster <31218426+jwebster7@users.noreply.github.com>
2022-08-11 16:08:01 -0500

    Add test files for relevant packages
    This commit adds empty test files so all relevant packages are factored into coverage reports.

---
+++ logger/logger_test.go
@@ -0,0 +1,17 @@
+// Copyright 2022 NetApp, Inc. All Rights Reserved.
+
+package logger
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func TestMain(m *testing.M) {
+	// Disable any standard log output
+	log.SetOutput(ioutil.Discard)
+	os.Exit(m.Run())
+}
