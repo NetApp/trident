c7e412e4dd8f8f9df1d5a0658b82518973f80da6
Shubham Phadnis <42840905+sphadnis007@users.noreply.github.com>
2022-09-01 19:44:02 +0530

    Added unit tests for frontend/common module

--- frontend/common/volumes_test.go
+++ frontend/common/volumes_test.go
@@ -3,12 +3,28 @@
 package common
 
 import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"os"
 	"testing"
 
+	"github.com/golang/mock/gomock"
+	log "github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/netapp/trident/config"
+	mockapi "github.com/netapp/trident/mocks/mock_core"
+	"github.com/netapp/trident/storage"
+	sa "github.com/netapp/trident/storage_attribute"
+	storageclass "github.com/netapp/trident/storage_class"
 )
+
+func TestMain(m *testing.M) {
+	// Disable any standard log output
+	log.SetOutput(ioutil.Discard)
+	os.Exit(m.Run())
+}
 
 func TestCombineAccessModes(t *testing.T) {
 	accessModesTests := []struct {
@@ -37,4 +53,85 @@ func TestCombineAccessModes(t *testing.T) {
 		accessMode := CombineAccessModes(tc.accessModes)
 		assert.Equal(t, tc.expected, accessMode, "Access Modes not combining as expected!")
 	}
+}
+
+func TestGetStorageClass(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	o := mockapi.NewMockOrchestrator(ctrl)
+
+	ctx := context.Background()
+	backendAndPools1 := "backend1:pool1,pool2"
+	backendAndPools2 := "backend2:pool3,pool4"
+	options := map[string]string{
+		sa.StoragePools:           backendAndPools1,
+		sa.AdditionalStoragePools: backendAndPools2,
+		"encryption":              "true",
+	}
+
+	// sa.StoragePools and sa.AdditionalStoragePools gets deleted from the "options" map
+	o.EXPECT().GetStorageClass(ctx, gomock.Any()).Return(nil, fmt.Errorf("error"))
+	_, err := GetStorageClass(ctx, options, o)
+	assert.Error(t, err, "GetStorageClass from orchestrator returned storage class")
+
+	// GetStorageClass from orchestrator returns matching storageclass from cache
+	sce := &storageclass.External{Config: &storageclass.Config{Name: "basicsc"}}
+	o.EXPECT().GetStorageClass(ctx, gomock.Any()).Return(sce, nil)
+	config, err := GetStorageClass(ctx, options, o)
+	assert.NoError(t, err, "GetStorageClass from orchestrator returned error")
+	assert.Equal(t, "basicsc", config.Name, "Found different storage class in orchestrator")
+
+	// AddStorageClass from orchestrator adds newly created storageclass in cache
+	o.EXPECT().GetStorageClass(ctx, gomock.Any()).Return(nil, nil)
+	o.EXPECT().AddStorageClass(ctx, gomock.Any()).Return(sce, nil)
+	config, err = GetStorageClass(ctx, options, o)
+	assert.NoError(t, err, "Failed to add storage class to orchestrator")
+	assert.Equal(t, "basicsc", config.Name, "Created storage class with different name")
+
+	// AddStorageClass from orchestrator fails in creating/adding the new storageclass to cache
+	o.EXPECT().GetStorageClass(ctx, gomock.Any()).Return(nil, nil)
+	o.EXPECT().AddStorageClass(ctx, gomock.Any()).Return(nil, fmt.Errorf("error"))
+	_, err = GetStorageClass(ctx, options, o)
+	assert.Error(t, err, "Added storage class to orchestrator")
+
+	// error condition for CreateBackendStoragePoolsMapFromEncodedString for sa.StoragePools
+	options[sa.StoragePools] = "backend1"
+	_, err = GetStorageClass(ctx, options, o)
+	assert.Error(t, err, "StoragePools storage attribute is correctly encoded")
+	delete(options, sa.StoragePools)
+
+	// error condition for CreateBackendStoragePoolsMapFromEncodedString for sa.AdditionalStoragePools
+	options[sa.AdditionalStoragePools] = "backend1"
+	_, err = GetStorageClass(ctx, options, o)
+	assert.Error(t, err, "AdditionalStoragePools storage attribute is correctly encoded")
+	delete(options, sa.AdditionalStoragePools)
+
+	// error condition for CreateAttributeRequestFromAttributeValue (error is only logged in this case)
+	options["dummy"] = "dummy"
+	o.EXPECT().GetStorageClass(ctx, gomock.Any()).Return(nil, fmt.Errorf("error"))
+	_, err = GetStorageClass(ctx, options, o)
+	assert.Error(t, err, "Correct attribute is added to storage attribute map")
+}
+
+func TestGetVolumeConfig(t *testing.T) {
+	dummyString := "dummy"
+	dummyMap := make([]map[string]string, 0)
+	expected := &storage.VolumeConfig{
+		Name:                dummyString,
+		Size:                "100",
+		StorageClass:        dummyString,
+		Protocol:            config.Protocol(dummyString),
+		AccessMode:          config.AccessMode(dummyString),
+		VolumeMode:          config.VolumeMode(dummyString),
+		RequisiteTopologies: dummyMap,
+		PreferredTopologies: dummyMap,
+	}
+
+	vol, err := GetVolumeConfig(dummyString, dummyString, 100, map[string]string{},
+		config.Protocol(dummyString), config.AccessMode(dummyString), config.VolumeMode(dummyString),
+		dummyMap, dummyMap)
+
+	assert.Nil(t, err, "Error while creating VolumeConfig object")
+	assert.Equal(t, expected, vol, "VolumeConfig object does not match")
 }
