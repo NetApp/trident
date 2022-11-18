ab2611051624a1b059cabded57130686a94a65cc
benclark0 <52842793+benclark0@users.noreply.github.com>
2022-11-18 18:21:06 -0500

    Update trident version to 23.01

--- operator/controllers/orchestrator/installer/installer_test.go
+++ operator/controllers/orchestrator/installer/installer_test.go
@@ -55,7 +55,7 @@ func createTestLabels() map[string]string {
 	labels := make(map[string]string)
 	labels[appLabelKey] = appLabelValue
 	labels[K8sVersionLabelKey] = "v1.21.8"
-	labels[TridentVersionLabelKey] = "v22.10.0"
+	labels[TridentVersionLabelKey] = "v23.01.0"
 
 	return labels
 }
