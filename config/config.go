27f739f0d0b8b0ef914e774b1f700838b4d16940
agagan <110954204+agagan@users.noreply.github.com>
2023-09-01 06:48:56 +0530

    changed trident version to 23.07.1
    changed trident version to 23.07.1

--- config/config.go
+++ config/config.go
@@ -39,7 +39,7 @@ const (
 	OrchestratorName                 = "trident"
 	OrchestratorClientName           = OrchestratorName + "ctl"
 	OrchestratorAPIVersion           = "1"
-	DefaultOrchestratorVersion       = "23.07.0"
+	DefaultOrchestratorVersion       = "23.07.1"
 	PersistentStoreBootstrapAttempts = 30
 	PersistentStoreBootstrapTimeout  = PersistentStoreBootstrapAttempts * time.Second
 	PersistentStoreTimeout           = 10 * time.Second
