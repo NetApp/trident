cb68cb389d9dea65eff2d2509b4784827bfbfe5c
agagan <110954204+agagan@users.noreply.github.com>
2024-06-28 13:41:12 +0530

    Auto-support version bump to 24.06
    Auto-support version bump to 24.06

--- config/config.go
+++ config/config.go
@@ -252,7 +252,7 @@ var (
 	}
 
 	// DefaultAutosupportImage default image used by tridentctl and operator for asup sidecar
-	DefaultAutosupportImage = "docker.io/netapp/trident-autosupport:24.02"
+	DefaultAutosupportImage = "docker.io/netapp/trident-autosupport:24.06"
 
 	// DefaultACPImage default image used by tridentctl and operator for acp sidecar
 	DefaultACPImage = "cr.astra.netapp.io/astra/trident-acp:24.06.0"
