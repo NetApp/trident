9ea9c6948c8e75a0a2487edd6030437a7f33b45e
reederc42 <reederc42@gmail.com>
2025-11-11 15:46:03 -0800

    Bumps Trident version to 25.06.3 and updates NOTICE

--- operator/controllers/orchestrator/installer/installer_test.go
+++ operator/controllers/orchestrator/installer/installer_test.go
@@ -62,7 +62,7 @@ func createTestLabels() map[string]string {
 	labels := make(map[string]string)
 	labels[appLabelKey] = appLabelValue
 	labels[K8sVersionLabelKey] = "v1.28.8"
-	labels[TridentVersionLabelKey] = "v25.06.2"
+	labels[TridentVersionLabelKey] = "v25.06.3"
 
 	return labels
 }
