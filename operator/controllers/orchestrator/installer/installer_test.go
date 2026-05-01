dd646cd7788bf4617255abaea603a82809dc1550
Tori Revilla <52927195+torirevilla@users.noreply.github.com>
2026-05-01 13:40:29 -0400

    [26.02.1] Version bump and notice file

--- operator/controllers/orchestrator/installer/installer_test.go
+++ operator/controllers/orchestrator/installer/installer_test.go
@@ -74,7 +74,7 @@ func createTestLabels() map[string]string {
 	labels := make(map[string]string)
 	labels[appLabelKey] = appLabelValue
 	labels[K8sVersionLabelKey] = "v1.28.8"
-	labels[TridentVersionLabelKey] = "v26.02.0"
+	labels[TridentVersionLabelKey] = "v26.02.1"
 
 	return labels
 }
